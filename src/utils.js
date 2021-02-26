a0388686b4f57dbdab076d5c0a80ef5ced00f78a
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2021-02-26 02:23:58 -0800

    Added util functions to fetch movie data from api

---
+++ src/utils.js
@@ -0,0 +1,19 @@
+export const getMoviesByName = async (search) => {
+    const baseUrl = 'http://www.omdbapi.com'
+    const apiKey = 'ADD YOUR API KEY HERE'
+    const url = `${baseUrl}/?apikey=${apiKey}&s=${search}`
+    
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+};
+
+export const getMovieDetailsById = async (movieId) => {
+    const baseUrl = 'http://www.omdbapi.com'
+    const apiKey = 'ADD YOUR API KEY HERE'
+    const url = `${baseUrl}/?apikey=${apiKey}&i=${movieId}`
+
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+};
